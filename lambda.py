c496ff8c61dd642ed7103af9dfbb752a4e71e6a2
EduardoFreitas <eduardo.freitas.cp@gmail.com>
2018-05-25 00:06:24 -0300

    fix lambda file

--- lambda.py
+++ lambda.py
@@ -1,18 +1,21 @@
 from __future__ import print_function
+import jobs.response
+import jobs.request
 
 
 def handler(event, context):
-    print("jobs: {}".format(event['call']))
-    # if event['call'] == 'request':
-    #     request.request_schedule()
-    # if event['call'] == 'result':
-    #     result.retrieve()
+    call = event['call']
+    print('Called: {}'.format(call))
+    if call == 'Request':
+        jobs.request.request_schedule()
+    if call == 'Response':
+        jobs.response.response_schedule()
 
-    return "jobs: {} Success".format(event['call'])
+    print('Success')
 
 
 if __name__ == '__main__':
     event_call = {
-        'call': 'request'
+        'call': 'Response'
     }
     handler(event_call, {})
